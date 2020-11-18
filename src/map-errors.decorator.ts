b45dce94e4399893b08ff0b3c61776e7110067ba
AlexXanderGrib <pockomhadzzop@gmail.com>
2020-11-18 13:20:37 +0300

    refactor(map-errors): Transformed getters to values

--- src/map-errors.decorator.ts
+++ src/map-errors.decorator.ts
@@ -1,20 +1,16 @@
 export function MapErrors(mapper: (error: Error) => Error): MethodDecorator {
   return function (_target, _prop, desc) {
-    const original = (desc.value as any) as Function;
+    const fn = (desc.value as any) as Function;
 
-    return {
-      get: function () {
-        const context = this as any;
-
-        return (...args: any[]) => {
-          try {
-            return original.apply(context, args);
-          } catch (error) {
-            throw mapper(error);
-          }
-        };
+    (desc as any).value = function (...args: any[]) {
+      try {
+        return fn.apply(this, args);
+      } catch (error) {
+        throw mapper(error);
       }
-    } as any;
+    };
+
+    return desc;
   };
 }
 
@@ -22,20 +18,16 @@ export function MapErrorsAsync(
   mapper: (error: Error) => Error | Promise<Error>
 ): MethodDecorator {
   return function (_target, _prop, desc) {
-    const original = (desc.value as any) as Function;
+    const fn = (desc.value as any) as Function;
 
-    return {
-      get: function () {
-        const context = this as any;
-
-        return async (...args: any[]) => {
-          try {
-            return await original.apply(context, args);
-          } catch (error) {
-            throw await mapper(error);
-          }
-        };
+    (desc as any).value = async function (...args: any[]) {
+      try {
+        return await fn.apply(this, args);
+      } catch (error) {
+        throw await mapper(error);
       }
-    } as any;
+    };
+
+    return desc;
   };
 }
