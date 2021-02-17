7e225c5e8a48ee36a3b5057ba06133a06dbe6257
AlexXanderGrib <me@alexxgrib.me>
2021-02-17 13:26:34 +0300

    fix(): Updated index exports

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ export * from "./di";
 export * from "./hooks";
 export * from "./logger";
 export * from "./map-errors";
+export * from "./memo";
 export * from "./meta";
 export * from "./mixin";
+export * as _sdk from "./sdk";
--- src/sdk.ts
+++ src/sdk.ts
@@ -61,10 +61,10 @@ export function methodDecorator(
  *
  */
 export function classDecorator(
-  function_: (parameters: { target: Function; proto: any; name: string }) => void
+  function_: (parameters: { target: Function; prototype: any; name: string }) => void
 ): ClassDecorator {
   return (target) =>
-    function_({ target, proto: target.prototype, name: target.name });
+    function_({ target, prototype: target.prototype, name: target.name });
 }
 
 type ParameterReplacementParameters = MethodReplacementParameters & {
